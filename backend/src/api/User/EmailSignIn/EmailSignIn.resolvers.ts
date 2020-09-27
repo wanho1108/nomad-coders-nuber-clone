7a2450f554ee242791c205e7b75bec342d940bed
wanho1108 <wanho1108@gmail.com>
2020-09-27 22:16:43 +0900

    #2.32 EmailSignIn Resolver part Two

--- backend/src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
+++ backend/src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -5,13 +5,27 @@ import { Resolvers } from "src/types/resolvers";
 const resolvers: Resolvers = {
   Mutation: {
     EmailSignIn: async (_, args: EmailSignInMutationArgs): Promise<EmailSignInResponse> => {
-      const { email } = args;
+      const { email, password } = args;
       try {
         const user = await User.findOne({ email })
         if (!user) {
           return {
             ok: false,
             error: "No User with that email",
+            token: null
+          }
+        }
+        const checkPassword = await user.comparePassword(password);
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: "Comming soon"
+          }
+        } else {
+          return {
+            ok: false,
+            error: "Wrong password",
             token: null
           }
         }
