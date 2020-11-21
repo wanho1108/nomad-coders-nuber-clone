7533b97570bb074269b540130a5cfb5fcd16e79d
wanho1108 <wanho1108@gmail.com>
2020-11-21 18:42:42 +0900

    오탈자 수정

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -2,7 +2,7 @@ import Mailgun from "mailgun-js";
 
 const mailgunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || "",
-  domain: "sandbox7c88a1a2bb644666a7c05bad8d352089.mailgun.org"
+  domain: "sandbox7c88a1a2bb644666a7c05bad8d352089.mailgun.org",
 });
 
 const sendEmail = (subject: string, body: string) => {
@@ -10,10 +10,10 @@ const sendEmail = (subject: string, body: string) => {
     from: "wanho1108@gmail.com",
     to: "wanho1108@gmail.comn",
     subject,
-    html: body
+    html: body,
   };
 
-  return mailgunClient.message().send(emailData);
+  return mailgunClient.messages().send(emailData);
 };
 
 export const sendVerificationEmail = (fullName: string, key: string) => {
