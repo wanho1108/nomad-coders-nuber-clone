0657dde67a76b4617026e4929823c12578beb9b6
wanho1108 <wanho1108@gmail.com>
2020-12-14 04:19:47 +0900

    #2.53 CompleteEmailVerification Resolver

---
+++ backend/src/api/User/CompleteEmailVerification/CompleteEmailVerification.resolvers.ts
@@ -0,0 +1,52 @@
+import { Resolvers } from "@/types/resolvers";
+import privateResolver from "@/utils/privateResolver";
+import User from "@/entities/User";
+import { CompleteEmailVerificationMutationArgs, CompleteEmailVerificationResponse } from "@/types/graphql";
+import Verification from "@/entities/Verification";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    CompleteEmailVerification: privateResolver(
+      async (_, args: CompleteEmailVerificationMutationArgs, { req }): Promise<CompleteEmailVerificationResponse> => {
+        const user: User = req.user;
+        const { key } = args;
+
+        if (user.email) {
+          try {
+            const verification = await Verification.findOne({
+              key,
+              payload: user.email,
+            });
+
+            if (verification) {
+              user.verifieddEmail = true;
+              user.save();
+
+              return {
+                ok: true,
+                error: null,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Cant verify email",
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "No email to verify",
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
