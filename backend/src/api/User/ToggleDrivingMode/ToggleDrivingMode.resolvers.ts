738624682bbfe1638136f478e3732ea57ca308b3
wanho1108 <wanho1108@gmail.com>
2020-12-19 01:50:32 +0900

    #2.57 ToggleDrivingMode Resolver

---
+++ backend/src/api/User/ToggleDrivingMode/ToggleDrivingMode.resolvers.ts
@@ -0,0 +1,22 @@
+import { Resolvers } from "@/types/resolvers";
+import privateResolver from "@/utils/privateResolver";
+import User from "@/entities/User";
+import { ToggleDrivingModeResponse } from "@/types/graphql";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    ToggleDrivingMode: privateResolver(
+      async (_, __, { req }): Promise<ToggleDrivingModeResponse> => {
+        const user: User = req.user;
+        user.isDriving = !user.isDriving;
+        user.save();
+        return {
+          ok: true,
+          error: null,
+        };
+      }
+    ),
+  },
+};
+
+export default resolvers;
