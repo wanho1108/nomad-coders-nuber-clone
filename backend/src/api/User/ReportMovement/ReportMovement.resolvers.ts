0b29ff6ee01e2b7e5832ab694541ea4717970511
wanho1108 <wanho1108@gmail.com>
2021-02-10 11:00:52 +0900

    #2.66 DriversSubscription part Two

--- backend/src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ backend/src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,18 +1,19 @@
-import { Resolvers } from "@/types/resolvers";
+import {Resolvers} from "@/types/resolvers";
 import privateResolver from "@/utils/privateResolver";
 import User from "@/entities/User";
 import cleanNullArgs from "@/utils/cleanNullArg";
-import { ReportMovementMutationArgs, ReportMovementResponse } from "@/types/graphql";
+import {ReportMovementMutationArgs, ReportMovementResponse} from "@/types/graphql";
 
 const resolvers: Resolvers = {
   Mutation: {
     ReportMovement: privateResolver(
-      async (_, args: ReportMovementMutationArgs, { req }): Promise<ReportMovementResponse> => {
+      async (_, args: ReportMovementMutationArgs, {req, pubSub}): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
 
         try {
-          await User.update({ id: user.id }, { ...notNull });
+          await User.update({id: user.id}, {...notNull});
+          pubSub.publish('driverUpdate', {DriversSubscription: user});
 
           return {
             ok: true,
